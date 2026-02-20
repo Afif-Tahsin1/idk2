7120e9e573f6a92c45f6b8d6fc1e569eece829cf
hhh <jareen0512@gmail.com>
2026-02-20 19:49:31 +0600

    1

---
+++ index.py
@@ -0,0 +1,14 @@
+tuplex = ("tuple", False, 1, 3.2)
+print(tuplex)
+
+tuplex = (4, 6, 2, 8, 3, 1)
+print(tuplex)
+tuplex = tuplex + (9,)
+print(tuplex)
+tuple1 = (50, 160, 70, 80)
+print(tuple1.count(50))
+tuplex = (2, 4, 3, 54, 6, 7, 8, 9)
+_slice = tuplex[3: 5]
+print(_slice)
+_slice = tuplex[:6]
+print(_slice)
